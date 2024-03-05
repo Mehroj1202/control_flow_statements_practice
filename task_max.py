e0c7d3cb58205d12e21e96fc45372d5fc6ade916
Mehroj1202 <iphonexseria@gmail.com>
2024-03-05 16:48:59 +0500

    task_mask.py

--- task_max.py
+++ task_max.py
@@ -9,7 +9,13 @@ def find_max(a,b,c):
     return:
         int
     """
-    return 0
+    if a<b<c:
+        return c
+    if a<c<b:
+        return b
+    if b<a<c:
+        return c
+print(find_max(-1,12,3))
 
 # Example:
 # find_max(1,2,3) -> 3
@@ -25,7 +31,14 @@ def find_max_idx(a,b,c):
     return:
         int
     """
-    return 0
+    if a<b<c:
+        return 3
+    if a<c<b:
+        return 2
+    if b<c<a:
+        return 1
+print(find_max_idx(1,3,4))
+print(find_max_idx(-1,12,3))
 
 # Example:
 # find_max_idx(10,2,13) -> 3
